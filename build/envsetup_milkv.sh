bfdc002e69c2fc3fe87de5061c5b866649393cd1
HenryChaing <n96121210@gs.ncku.edu.tw>
2025-06-17 22:47:46 +0800

    Add ThreadX as Git Submodule
    Integrated the ThreadX RTOS to run on the C906L core as a git submodule.
    The Buildroot SDK build flow has been updated to compile ThreadX
    (without FreeRTOS) as the secondary operating system.

--- build/envsetup_milkv.sh
+++ build/envsetup_milkv.sh
@@ -765,7 +765,8 @@ function cvi_setup_env()
   FSBL_PATH="$TOP_DIR"/fsbl
   ATF_PATH="$TOP_DIR"/arm-trusted-firmware
   UBOOT_PATH="$TOP_DIR/$UBOOT_SRC"
-  FREERTOS_PATH="$TOP_DIR"/freertos
+  # FREERTOS_PATH="$TOP_DIR"/freertos
+  FREERTOS_PATH="$TOP_DIR"/threadx
   ALIOS_PATH="$TOP_DIR"/alios
   KERNEL_PATH="$TOP_DIR"/"$KERNEL_SRC"
   OSDRV_PATH="$TOP_DIR"/osdrv
